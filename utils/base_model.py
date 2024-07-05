0296aff1662e189200b5f602e8469e5a6333b960
Ali Bagheri <khodealib@gmail.com>
2024-07-05 23:15:33 +0330

    feat: implement BaseModel
    This model contains created_at and updated_at

---
+++ utils/base_model.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """
+    This is an abstract base model class that provides common fields for all models.
+
+    Attributes:
+    created_at (DateTimeField): Automatically set to the current date and time when an object is first created.
+    updated_at (DateTimeField): Automatically set to the current date and time whenever an object is saved.
+
+    Methods:
+    None
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    """
+    Automatically set to the current date and time when an object is first created.
+    """
+
+    updated_at = models.DateTimeField(auto_now=True)
+    """
+    Automatically set to the current date and time whenever an object is saved.
+    """
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
+        """
+        This class is marked as abstract, meaning it cannot be instantiated directly.
+        Ordering is set to sort objects by created_at in descending order.
+        """
