d098d939a94325be5a0f5a894d20dbff6c6b2958
Ali Bagheri <khodealib@gmail.com>
2024-07-06 01:43:32 +0330

    feat: add appointments model.

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+
+from appointments.models import Appointment
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("doctor", "patient", "start_date", "attended")
+    list_filter = ("created_at", "updated_at", "attended")
+    search_fields = (
+        "doctor__user__first_name",
+        "doctor__user__last_name",
+        "patient__user__first_name",
+        "patient__user__last_name",
+        "start_date",
+        "attended",
+    )
