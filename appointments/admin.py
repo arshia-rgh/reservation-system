a472d392ddecfe2ddad06db61ce71668560a7fa0
Ali Bagheri <khodealib@gmail.com>
2024-07-06 03:07:42 +0330

    feat: add docstring for appointment admin.

--- appointments/admin.py
+++ appointments/admin.py
@@ -5,8 +5,31 @@ from appointments.models import Appointment
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
+    """
+    Custom admin model for the Appointment model.
+
+    Attributes:
+    - list_display: Fields to display in the change list page of the admin.
+    - list_filter: Fields to filter the change list page of the admin.
+    - search_fields: Fields to search in the change list page of the admin.
+    """
+
     list_display = ("doctor", "patient", "start_date", "attended")
+    """
+    Fields to display in the change list page of the admin.
+
+    Returns:
+    - tuple: A tuple of field names.
+    """
+
     list_filter = ("created_at", "updated_at", "attended")
+    """
+    Fields to filter the change list page of the admin.
+
+    Returns:
+    - tuple: A tuple of field names.
+    """
+
     search_fields = (
         "doctor__user__first_name",
         "doctor__user__last_name",
@@ -15,3 +38,9 @@ class AppointmentAdmin(admin.ModelAdmin):
         "start_date",
         "attended",
     )
+    """
+    Fields to search in the change list page of the admin.
+
+    Returns:
+    - tuple: A tuple of field names.
+    """
