8d966c4489bb9bf6dec9e8003d31fc4149a2d3d9
arshia <arshiarezagholi1212@gmail.com>
2024-07-07 16:04:30 +0330

    basic implemention of register

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import CreateView
+
+from accounts.forms import RegisterForm
+
+
+class Register(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = RegisterForm
