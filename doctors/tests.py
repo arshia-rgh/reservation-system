e6a87f7c1bb8ca10df908975e41dc7f93151d0ba
Ali Bagheri <khodealib@gmail.com>
2024-07-11 00:05:14 +0330

    fix: fix accounts urls path name

--- doctors/tests.py
+++ doctors/tests.py
@@ -8,22 +8,22 @@ from doctors.models import Doctor, Speciality
 class DoctorDetailViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        self.speciality = Speciality.objects.create(name='Cardiology')
-        self.doctor = baker.make(Doctor, speciality=self.speciality, first_name='John', last_name='Doe')
+        self.speciality = Speciality.objects.create(name="Cardiology")
+        self.doctor = baker.make(Doctor, speciality=self.speciality, first_name="John", last_name="Doe")
 
     def test_doctor_detail_view_renders_correct_template(self):
         response = self.client.get(self.doctor.get_absolute_url())
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'doctors/detail.html')
+        self.assertTemplateUsed(response, "doctors/detail.html")
 
     def test_doctor_detail_view_contains_expected_doctor_name(self):
         response = self.client.get(self.doctor.get_absolute_url())
-        self.assertContains(response, 'John Doe')
+        self.assertContains(response, "John Doe")
 
     def test_doctor_detail_view_contains_expected_doctor_specialty(self):
         response = self.client.get(self.doctor.get_absolute_url())
-        self.assertContains(response, 'Cardiology')
+        self.assertContains(response, "Cardiology")
 
     def test_doctor_detail_view_returns_404_for_invalid_pk(self):
-        response = self.client.get(reverse('doctors:detail', args=[999]))
+        response = self.client.get(reverse("doctors:detail", args=[999]))
         self.assertEqual(response.status_code, 404)
